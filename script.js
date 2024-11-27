85568bccc479b01a90842017e2525eb5b1cca663
Ashneet Kaur <83127542+ashneeeet@users.noreply.github.com>
2024-11-28 01:00:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('mousemove', function(e) {
+    createSparkle(e.pageX, e.pageY);
+});
+
+function createSparkle(x, y) {
+    const sparkle = document.createElement('div');
+    sparkle.className = 'sparkle';
+    document.querySelector('.sparkle-container').appendChild(sparkle);
+
+    sparkle.style.left = x + 'px';
+    sparkle.style.top = y + 'px';
+
+    setTimeout(() => {
+        sparkle.remove();
+    }, 500);
+}
